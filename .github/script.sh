baf45b3c9ca489832fe053e89c176cbf0e7fcba1
IamDum <ramanjot.pro@gmail.com>
2021-03-23 20:56:13 +0100

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -99,6 +99,10 @@ INITIAL_COLUMN_NAME="$INPUT_COLUMN_NAME"
 
 
 PROJECT_ID=$(find_project_id "$PROJECT_TYPE" "$PROJECT_URL")
+echo "$PROJECT_ID"
+
+echo "HHHHHHHH999"
+
 INITIAL_COLUMN_ID=$(find_column_id "$PROJECT_ID" "${INITIAL_COLUMN_NAME:?<Error> required this environment variable}")
 
 echo "$INITIAL_COLUMN_ID"
