e1121bf6d46a4730a3b8c13a9ddd6b6ce0900585
IamDum <ramanjot.pro@gmail.com>
2021-03-26 15:54:54 +0100

    Update hh.sh

--- .github/hh.sh
+++ .github/hh.sh
@@ -49,9 +49,9 @@ find_project_id() {
            -H 'Accept: application/vnd.github.inertia-preview+json' \
            "$_ENDPOINT")
 
-  _PROJECTS=${_PROJECTS%$'\n'*}
 
-  _PROJECTID=$(echo "$_PROJECTS" | jq -r ".[] | select(.html_url == \"$_PROJECT_URL\").id")
+
+  _PROJECTID=$(echo "$_PROJECTS" | jq -r ".[] | tr -d '\n' | select(.html_url == \"$_PROJECT_URL\").id") 
 
   if [ "$_PROJECTID" != "" ]; then
     echo "$_PROJECTID"
