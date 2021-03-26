43527083a5184d2a68666df040b9ccfa451ee7ac
IamDum <ramanjot.pro@gmail.com>
2021-03-26 17:33:04 +0100

    Update hh.sh

--- .github/hh.sh
+++ .github/hh.sh
@@ -50,8 +50,7 @@ find_project_id() {
            -H 'Accept: application/vnd.github.inertia-preview+json' \
            "$_ENDPOINT")
 
-  _CLEAN=$(echo $_PROJECTS | tr -d '\r')
-  _CLEAN=$(echo $_CLEAN | tr -d '\n')  
+  _CLEAN=$(echo $_PROJECTS | tr '\r\n' ' ')
   #_CLEANN=$(echo $_CLEAN | sed -z 's/\r/-----/g')
   #_CLEANN=$(echo $_CLEAN | sed -z 's/\n/-----/g')
 
