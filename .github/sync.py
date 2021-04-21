b12fdce7f979fd384863d108018302d1fcc9b33c
IamDum <ramanjot.pro@gmail.com>
2021-04-21 13:26:05 +0200

    Update sync.py

--- .github/sync.py
+++ .github/sync.py
@@ -9,7 +9,7 @@ length = len(sys.argv)
 
 if length < 3:
     print('This script requires two command line areguments\n1. Github access token\n2. Release milestone')
-    sys.exit()u
+    sys.exit()
 auth_token = sys.argv[1]
 
 github = Github(login_or_token=auth_token, per_page=10)
