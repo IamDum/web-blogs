a18f8b627ab789f4fe5ab7f85c394334a198525c
IamDum <ramanjot.pro@gmail.com>
2021-04-21 12:56:23 +0200

    Update sync.py

--- .github/sync.py
+++ .github/sync.py
@@ -1,1 +1,24 @@
-print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+from __future__ import print_function
+
+import sys
+from collections import defaultdict
+from github import Github
+
+
+length = len(sys.argv)
+
+if length < 3:
+    print('This script requires two command line areguments\n1. Github access token\n2. Release milestone')
+    sys.exit()
+
+
+auth_token = sys.argv[1]
+
+github = Github(login_or_token=auth_token, per_page=self.PAGE_ITEM_COUNT)
+repo_tmp = github.get_repo("IamDum/web-blogs")
+
+
+print(repo_tmp)
+
+return repo_tmp
+
